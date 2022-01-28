95ad567ee65f2e1ac8808ac797406904f3a267a5
Adaminite <niazi.omark@gmail.com>
2022-01-28 16:11:57 -0500

    Add header and footer

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import renderMainDiv from "./mainwindow.js";
+import header from "./header.js";
+import footer from "./footer.js";
 import '../styles/style.css';
 
 const displayController = ( () => {
@@ -6,7 +8,9 @@ const displayController = ( () => {
     const body = document.querySelector('body');
 
     const renderPage = () => {
+        body.appendChild(header);
         body.appendChild( renderMainDiv() );
+        body.appendChild(footer);
     }
 
     return {renderPage};
