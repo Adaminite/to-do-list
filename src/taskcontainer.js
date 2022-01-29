1ee991169bc1c39309779544798c539f75aab684
Adaminite <niazi.omark@gmail.com>
2022-01-28 19:16:37 -0500

    Add sub header

--- src/taskcontainer.js
+++ src/taskcontainer.js
@@ -1,8 +1,20 @@
-function items(){
+
+
+function subHeader(){
+    let subHeader = document.createElement('h2');
+    subHeader.textContent = "Tasks";
+    subHeader.id = "task-subheader";
+    
+
+    return subHeader;
+}
+
+function taskContainer(){
     let itemDiv = document.createElement('div');
     itemDiv.id = "items";
 
+    itemDiv.appendChild( subHeader() );
     return itemDiv;
 }
 
-export default items;
+export default taskContainer;
