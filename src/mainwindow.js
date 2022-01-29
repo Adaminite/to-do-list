36e4e6d05a8500d37eb6314f02589fa992c69282
Adaminite <niazi.omark@gmail.com>
2022-01-28 22:57:02 -0500

    Add button event

--- src/mainwindow.js
+++ src/mainwindow.js
@@ -2,12 +2,14 @@ import {sidebar} from "./sidebar.js";
 import taskContainer from "./taskcontainer.js";
 
 
+
 function renderMainDiv() {
     let mainDiv = document.createElement('div');
     mainDiv.id = "content";
     
     mainDiv.appendChild( sidebar.renderSidebar() );
     mainDiv.appendChild( taskContainer() );
+    
     
     return mainDiv;
 }
