f9d07df7b8af2b909c0026a062a461bd469021bd
Adaminite <niazi.omark@gmail.com>
2022-01-29 15:01:36 -0500

    Create displayController

---
+++ src/tasks.js
@@ -0,0 +1,17 @@
+import createTask from './task.js';
+
+const tasks = ( () => {
+
+    const allTasks = [];
+
+    const addTask = (name, date) => {
+
+        const newTask = createTask(name, date);
+        allTasks.push(allTasks);
+
+        taskDiv = document.createElement('div');
+
+    }
+
+
+})();
